b5df4e16905ca087985d2a9050ff15683eb48a28
Biaobin Li <libb@ihep.ac.cn>
2021-02-20 15:08:31 +0800

    commit source.sh

---
+++ source.sh
@@ -0,0 +1,18 @@
+#Name: Biaobin Li
+#email: libb@ihep.ac.cn
+
+# usage, in command line, type:
+# source setup.sh
+# It will temporary add the following environment 
+# valuables to the path
+
+# get current path
+ROOTPATH=`pwd`
+export PATH=$ROOTPATH/utilities:$ROOTPATH/src:$PATH
+
+# if you want to write the env-variable to .zshrc/.bashrc
+echo " " >> ~/.zshrc
+echo "# install IMPACT-Z" >> ~/.zshrc
+echo "# =============" >> ~/.zshrc
+
+echo "export PATH=$ROOTPATH/utilities:$ROOTPATH/src:\$PATH" >> ~/.zshrc
