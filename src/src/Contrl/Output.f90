dcdf4997d630f3eeeb19d533059d3b4a77dee022
Biaobin Li <libb@ihep.ac.cn>
2021-07-17 19:45:17 +0800

    fort.26, comments for [rad] to [m].

--- src/src/Contrl/Output.f90
+++ src/src/Contrl/Output.f90
@@ -416,6 +416,9 @@
           write(25,104)z,y0*xl,yrms*xl,py0/gambet,pyrms/gambet,-ypy/epy,&
                        epy*xl,betay,gammay,etay,etayp,turn_ith
           write(26,100)z,z0*xt,zrms*xt,pz0*qmc,pzrms*qmc,-zpz/epz,epz*qmc*xt
+          !biaobin, if unit of [m] is wanted: [rad] -> [m]
+          !write(26,100)z,z0*xl,zrms*xl,pz0*qmc,pzrms*qmc,-zpz/epz,epz*qmc*xl
+
           write(27,100)z,glmax(1)*xl,glmax(2)/gambet,glmax(3)*xl,&
                        glmax(4)/gambet,glmax(5)*xt,glmax(6)*qmc
           write(28,101)z,npctmin,npctmax,nptot
